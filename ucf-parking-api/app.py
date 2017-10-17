18fc5ad6184fe8c433ef496f5afc52569cce9190
Jesse McBride <jm@ucf.edu>
2017-10-17 16:48:49 -0400

    Initial commit

---
+++ ucf-parking-api/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import jsonify
+
+from scraper import scrape
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/garages')
+@app.route('/api/v1/garages/')
+def garages():
+    return jsonify(scrape())
+
+
+@app.route('/api/v1/garages/<garage>')
+@app.route('/api/v1/garages/<garage>/')
+def garage(garage):
+    garage = filter(lambda x: x.name.lower() == garage.lower(), scrape())
+    return jsonify(garage)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ ucf-parking-api/scraper.py
@@ -0,0 +1,34 @@
+import math
+import urllib2
+from collections import namedtuple
+
+from bs4 import BeautifulSoup
+
+url = 'http://secure.parking.ucf.edu/GarageCount/'
+Garage = namedtuple('Garage', 'name available total percentage')
+
+
+def scrape():
+    page = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(page, 'html.parser')
+    rows = soup.find_all('tr', class_='dxgvDataRow_DevEx')
+
+    garages = []
+
+    for row in rows:
+        data = row.find_all(class_='dxgv')
+
+        garage = data[0].text.replace('Garage ', '')
+        available, total = tuple(map(int, data[1].text.strip().split('/')))
+        percentage = math.floor(100 - (float(available) / float(total)) * 100)
+
+        garages.append(
+            Garage(
+                name=garage,
+                available=available,
+                total=total,
+                percentage=percentage
+            )
+        )
+
+    return garages
